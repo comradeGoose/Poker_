38d7e37c8606772c85cfc905d9cf12d975d62758
Alexey Grebenyuk <vladimir-g2010@mail.ru>
2021-06-01 19:39:15 +0900

    Добавьте файлы проекта.

---
+++ Poker_/Card.cs
@@ -0,0 +1,44 @@
+﻿namespace Poker
+{
+    public class Card
+    {
+        public enum Suit
+        {
+            Черви = 1,
+            Трефы,
+            Бубны,
+            Пики
+        }
+
+        public enum Advantages
+        {
+            ace = 1,
+            Двойка,
+            Тройка,
+            Четверка,
+            Пятерка,
+            Шестерка,
+            Семерка,
+            Восьмерка,
+            Девятка,
+            Десятка,
+            Валет,
+            Королева,
+            Король
+        }
+
+        private Suit suit;
+        private Advantages advantages;
+
+        public Card(Suit suit, Advantages advantages)
+        {
+            this.suit = suit;
+            this.advantages = advantages;
+        }
+
+        public override string ToString()
+        {
+            return $"{suit.ToString()} - {advantages.ToString()}";
+        }
+    }
+}
